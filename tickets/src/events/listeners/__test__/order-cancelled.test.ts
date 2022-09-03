dff073b4aaf7917714c3483a3636686c565c1461
James Reinhardt <liverat@student.ubc.ca>
2022-09-03 13:30:34 -0700

    Added order cancellation functionality

---
+++ tickets/src/events/listeners/__test__/order-cancelled.test.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { OrderCancelledEvent, OrderStatus } from "@liverattickets/common";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 29,
+    userId: 'ajajsi',
+  });
+  ticket.set({orderId});
+  await ticket.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: orderId,
+    version: 0,
+    ticket: {
+      id: ticket.id,
+    }
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn()
+  };
+
+  return { listener, ticket, data, orderId, msg };
+}
+
+it('updates a ticket, publishes an event, and acks the message', async () => {
+  const  {listener, ticket, data, orderId, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.orderId).not.toBeDefined();
+  expect(msg.ack).toHaveBeenCalled();
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
