ac7d93b95cb9abf6b6893cd69b146c21e3b55b13
James Reinhardt <liverat@student.ubc.ca>
2022-09-04 19:37:26 -0700

    Skaffold working now

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,4 +1,4 @@
-import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@liverattickets/common";
+import { Listener, OrderCreatedEvent, Subjects } from "@liverattickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { expirationQueue } from "../../queues/expiration-queue";
@@ -7,14 +7,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: { id: string; version: number; status: OrderStatus; userId: string; expiresAt: string; ticket: { id: string; price: number; }; }, msg: Message) {
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log('Waiting this many milliseconds to process a job:', delay);
+    console.log('Waiting this many milliseconds to process the job:', delay);
     
-    await expirationQueue.add({
-      orderId: data.id
-    }, {
-      delay: delay,
+    await expirationQueue.add(
+    {
+      orderId: data.id,
+    },
+    {
+      delay,
     }
     );
 
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,17 +1,16 @@
-import { natsWrapper } from "./nats-wrapper";
-import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
-
-    if (!process.env.NATS_URL) {
-      throw new Error("NATS_URL must be defined");
-    }
-    if (!process.env.NATS_CLIENT_ID) {
-      throw new Error("NATS_CLIENT_ID must be defined");
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-      throw new Error("NATS CLUSTER_ID must be defined");
-    }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
 
   try {
     await natsWrapper.connect(
@@ -30,7 +29,6 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-
 };
 
 start();
