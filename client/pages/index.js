b0fffdccdc6646ab77b6890dc1da923e11593569
James Reinhardt <liverat@student.ubc.ca>
2022-09-09 10:29:31 -0700

    Updating client code

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,11 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({currentUser}) => {
   return currentUser? <h1>You are signed in</h1> : <h1>You are not signed in</h1>
 }
 
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser').catch(err => {
-    console.log(err.messsage);
-  });
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const {data} = await client.get('/api/tickets');
 
-  return data;
+  return { tickets: data };
 }
 
 export default LandingPage;
